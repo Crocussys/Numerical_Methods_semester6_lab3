6f7600b54798b7f49be3bb679865f558b6693bd5
Egor <47773467+Crocussys@users.noreply.github.com>
2023-04-30 00:34:37 +0300

    fix and finished python

--- python/main.py
+++ python/main.py
@@ -8,16 +8,12 @@ def f(x):
 
 def jacobi_1(x):
     x1, x2 = x[0], x[1]
-    return np.array([[3 * x2 ** 2 * x1 - 1, -x2 ** 3], [2 * x2, 3 * x1 ** 2]]) /\
+    return np.array([[3 * x2 ** 2 * x1 - 1, 2 * x2], [-x2 ** 3, 3 * x1 ** 2]]) /\
         (9 * x1 ** 3 * x2 ** 2 - 3 * x1 ** 2 + 2 * x2 ** 4)
 
 
 def is_end(x_prev, x, fx, err1, err2):
-    delta1, delta2 = 0, 0
-    for i in range(len(fx)):
-        val = abs(fx[i])
-        if val > delta1:
-            delta1 = val
+    delta1, delta2 = max(abs(fx)), 0
     for i in range(len(x)):
         if abs(x[i]) < 1:
             val = abs(x[i] - x_prev[i])
@@ -33,19 +29,30 @@ def is_end(x_prev, x, fx, err1, err2):
     return True
 
 
+def np_to_print(arr):
+    string = ""
+    for val in arr:
+        string += str(val) + ", "
+    string = "[" + string[:-2] + "]"
+    return string
+
+
 def main(x, max_i=1000, err1=1e-9, err2=1e-9):
-    i, fx = 1, f(x)
-    x_next = x - jacobi_1(x) @ fx
-    print("1:", end=" ")
-    while not is_end(x, x_next, fx, err1, err2) and i <= max_i:
-        fx = f(x)
-        x_next, x = x - jacobi_1(x) @ fx, x_next
+    x_prev = x.copy()
+    print(f"0: {np_to_print(x_prev)}")
+    i, fx = 1, f(x_prev)
+    x_next = x_prev - jacobi_1(x_prev) @ fx
+    print(f"1: {np_to_print(x_next)}", end=" ")
+    while not is_end(x_prev, x_next, fx, err1, err2) and i <= max_i:
+        x_prev = x_next
+        fx = f(x_prev)
+        x_next = x_prev - jacobi_1(x_prev) @ fx
         i += 1
-        print(f"{i}:", end=" ")
+        print(f"{i}: {np_to_print(x_next)}", end=" ")
     return x_next
 
 
 if __name__ == '__main__':
     x0 = np.array([1.2, 1.3])
     ans = main(x0)
-    print(f"Ответ {ans}")
+    print(f"Ответ {np_to_print(ans)}")
